cf30aea8c8e0d1f39f8d2c4500255fd3a8e8e73f
Elara <188837568+ElaraCrest@users.noreply.github.com>
2025-05-24 17:39:01 +0800

    d

---
+++ LostChocolate/Assets/NPC1.cs
@@ -0,0 +1,16 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NPC1 : MonoBehaviour
+{
+   public NPCDialogue dialogueData;
+   public GameObject dialoguePanel;
+    public TMP_Text dialogueText, nameText;
+    public Image portraitImage;
+
+    private int dialogueIndex;
+    private bool isTyping, isDialogueActive;
+
+   
+}
