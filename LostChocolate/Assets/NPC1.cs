5ff3d5dd14063f0183169199ec059991c8108d47
Elara <188837568+ElaraCrest@users.noreply.github.com>
2025-05-25 13:51:53 +0800

    f

--- LostChocolate/Assets/NPC1.cs
+++ LostChocolate/Assets/NPC1.cs
@@ -1,8 +1,9 @@
+using System.Collections;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class NPC1 : MonoBehaviour
+public class NPC1 : MonoBehaviour, IInteractable
 {
    public NPCDialogue dialogueData;
    public GameObject dialoguePanel;
@@ -12,5 +13,91 @@ public class NPC1 : MonoBehaviour
     private int dialogueIndex;
     private bool isTyping, isDialogueActive;
 
-   
+    public bool CanInteract()
+    {
+        return !isDialogueActive;
+    }
+
+    public void Interact()
+    {
+        if (dialogueData == null || (PauseController.IsGamePaused && !isDialogueActive))
+        {
+            return;
+        }
+
+        if (isDialogueActive)
+        {
+            NextLine();
+        }
+        else
+        {
+            StartDialogue();
+        }
+
+
+    }
+
+    void StartDialogue()
+    {
+        isDialogueActive = true;
+        dialogueIndex = 0;
+
+        nameText.SetText(dialogueData.name);
+        portraitImage.sprite = dialogueData.npcPortrait;
+
+        dialoguePanel.SetActive(true);
+        PauseController.SetPause(true);
+
+        StartCoroutine(TypeLine());
+    }
+
+    void NextLine()
+    {
+        if(isTyping)
+        {
+            //skip typing animation and show full line
+            StopAllCoroutines();
+            dialogueText.SetText(dialogueData.dialogueLines[dialogueIndex]);
+            isTyping = false;
+        }
+        else if (++dialogueIndex < dialogueData.dialogueLines.Length)
+        {
+            // if another line, type next line
+            StartCoroutine(TypeLine());
+        }
+        else
+        {
+            //end dialogue
+            EndDialogue();
+
+        }
+    }
+    IEnumerator TypeLine()
+    {
+        isTyping = true;
+        dialogueText.SetText("");
+
+        foreach (char letter in dialogueData.dialogueLines[dialogueIndex])
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(dialogueData.typingSpeed);
+        }
+
+        isTyping = false;
+
+        if(dialogueData.autoProgressLines.Length > dialogueIndex && dialogueData.autoProgressLines[dialogueIndex])
+        {
+            yield return new WaitForSeconds(dialogueData.autoProgressDelay);
+            NextLine();
+        }
+    }
+
+    public void EndDialogue()
+    {
+        StopAllCoroutines();
+        isDialogueActive = false;
+        dialogueText.SetText("");
+        dialoguePanel.SetActive(false);
+        PauseController.SetPause(false);
+    }
 }
